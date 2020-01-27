4d3f728c0ea8c0df3b5acf320c49df0ec1d3529d
Your Name <you@example.com>
2020-01-26 21:43:46 -0500

    export

---
+++ export.py
@@ -0,0 +1,19 @@
+import d6tflow
+import d6tflow.pipes
+
+import tasks
+
+cfg_pipe = 'pipename'
+cfg_profile = 'default'
+d6tflow.pipes.init(cfg_pipe,profile=cfg_profile, local_pipe=True)
+
+task = tasks.TaskGetData()
+d6tflow.run(task)
+
+d6tflow.pipes.FlowExport(task,cfg_pipe).generate()
+
+do_push = False
+if do_push:
+    d6tflow.pipes.init(cfg_pipe,profile=cfg_profile, reset=True)
+    pipe = d6tflow.pipes.get_pipe()
+    pipe.push_preview()

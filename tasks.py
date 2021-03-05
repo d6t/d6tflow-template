6dd173e043ebd64dd08132c516429dd78ab2b38d
mozin <mohsinvindhani@gmail.com>
2021-03-05 11:23:10 +0530

    luigi to d6tflow for all params

--- tasks.py
+++ tasks.py
@@ -1,5 +1,4 @@
 import d6tflow
-import luigi
 import sklearn, sklearn.datasets
 from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
@@ -21,7 +20,7 @@ class TaskGetData(d6tflow.tasks.TaskPqPandas):  # save dataframe as parquet, see
 
 @d6tflow.requires(TaskGetData) # define dependency. See https://d6tflow.readthedocs.io/en/latest/tasks.html
 class TaskPreprocess(d6tflow.tasks.TaskPqPandas):
-    do_preprocess = luigi.BoolParameter(default=cfg.do_preprocess) # parameter for preprocessing yes/no
+    do_preprocess = d6tflow.BoolParameter(default=cfg.do_preprocess) # parameter for preprocessing yes/no
 
     def run(self):
         df_train = self.input().load() # quickly load required data, see https://d6tflow.readthedocs.io/en/latest/tasks.html#load-input-data

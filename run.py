efe181863d15afe822f616b53daf3acf2c9b4719
Norman <citynorman@hotmail.com>
2024-03-31 11:38:01 -0400

    ux

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 import d6tflow
 import cfg, tasks
-import visualize
 
 params=dict()
-flow = d6tflow.Workflow(task=tasks.Process, params=params)
+task=tasks.Process
+
+flow = d6tflow.Workflow(task, params=params)
 flow.preview()
 flow.run()
